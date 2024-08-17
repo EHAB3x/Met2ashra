b1413338c74e29c77b214a9970fd0f21a019918b
Nabil Alex <nabilmohaed46@gmail.com>
2024-08-17 22:30:10 +0300

    handle of carouselCard

--- src/components/Met2ashara/Home/CarouselCard.tsx
+++ src/components/Met2ashara/Home/CarouselCard.tsx
@@ -6,17 +6,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@components/ui/carousel";
+import basics from "@assets/svg/basics.svg";
+import companies from "@assets/svg/companies.svg";
+import mentor from "@assets/svg/mentor.svg";
 
 export function CarouselCard() {
+  const data = [
+    { img: basics, title: "Programing Basic" },
+    { img: companies, title: "Companies" },
+    { img: mentor, title: "Mentor" },
+  ];
   return (
-    <Carousel className="w-full max-w-sm">
-      <CarouselContent className="-ml-1">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+    <Carousel className="w-[85%] mx-auto my-14 py-5">
+      <CarouselContent className="-ml-1 gap-4">
+        {data.map((content, index) => (
+          <CarouselItem key={index} className="pl-1 md:basis-2/5 lg:basis-1/3">
             <div className="p-1">
               <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
+                <CardContent className="flex aspect-square items-center justify-center p-1 bg-secondary flex-col md:max-w-[500px] max-w-[300px]">
+                  <h2>{content.title}</h2>
+                  <img src={content.img} />
                 </CardContent>
               </Card>
             </div>
--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,9 +1,11 @@
 import Hero from "@components/Met2ashara/Home/Hero";
+import CarouselCard from "@components/Met2ashara/Home/CarouselCard";
 
 const HomeLayout = () => {
   return (
     <>
       <Hero />
+      <CarouselCard />
     </>
   );
 };
