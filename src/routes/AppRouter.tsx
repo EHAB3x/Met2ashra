bb452512842b6e9733377ed04e384ac5000d60f7
Nabil Alex <nabilmohaed46@gmail.com>
2024-08-21 09:47:13 +0300

    add route for signup and login

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,18 +3,28 @@ import { QueryClient, QueryClientProvider } from "react-query";
 // Layouts
 import { HomeLayout, MainLayout } from "@layouts/index";
 // Pages
+import Login from "@pages/Login";
+import Signup from "@pages/Signup";
 // Router Functions
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
-    errorElement:<p>Error</p>,
+    // element: <MainLayout />,
+    errorElement: <p>Error</p>,
     children: [
       {
         index: true,
-        element:<HomeLayout />,
+        element: <HomeLayout />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
       },
     ],
   },
