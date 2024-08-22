dae2f307574f8008366623679fdf3b6ace5f44f0
EHAB3x <ehabmah6969@gmail.com>
2024-08-22 02:18:26 +0200

    fix layout problems

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -27,34 +27,52 @@ const Nav = () => {
         className="flex items-center gap-2 text-lg font-semibold md:text-base sm:flex-none md:flex-none lg:flex-none flex-1"
       >
         <Logo className="h-12 w-12" />
-        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#113573] to-[#4B97F4] font-bold text-[1.5rem]">Met2ashara</h1>
+        <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-[#113573] to-[#4B97F4] font-bold text-[1.5rem]">
+          Met2ashara
+        </h1>
       </NavLink>
 
       <nav className="hidden flex-col gap-8 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-8">
         <NavLink
           to="/"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
+          className={({ isActive }) =>
+            `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+              { isActive }
+            )}`
+          }
         >
           Home
         </NavLink>
 
         <NavLink
           to="/about-us"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
+          className={({ isActive }) =>
+            `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+              { isActive }
+            )}`
+          }
         >
           About Us
         </NavLink>
 
         <NavLink
           to="/contact"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
+          className={({ isActive }) =>
+            `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+              { isActive }
+            )}`
+          }
         >
           Contact
         </NavLink>
 
         <NavLink
           to="/roadmap"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
+          className={({ isActive }) =>
+            `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+              { isActive }
+            )}`
+          }
         >
           Roadmap
         </NavLink>
@@ -68,66 +86,75 @@ const Nav = () => {
           </Button>
         </SheetTrigger>
         <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              to="/"
-              className="flex items-center gap-2 text-lg font-semibold"
-            >
-              <Logo className="h-12 w-12" />
-              <span className="sr-only">Acme Inc</span>
-            </Link>
+          <nav className="grid gap-6 text-lg font-medium pt-[90px]">
             <NavLink
-          to="/"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
-        >
-          Home
-        </NavLink>
+              to="/"
+              className={({ isActive }) =>
+                `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+                  { isActive }
+                )}`
+              }
+            >
+              Home
+            </NavLink>
 
-        <NavLink
-          to="/about-us"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
-        >
-          About Us
-        </NavLink>
+            <NavLink
+              to="/about-us"
+              className={({ isActive }) =>
+                `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+                  { isActive }
+                )}`
+              }
+            >
+              About Us
+            </NavLink>
 
-        <NavLink
-          to="/contact"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
-        >
-          Contact
-        </NavLink>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+                  { isActive }
+                )}`
+              }
+            >
+              Contact
+            </NavLink>
 
-        <NavLink
-          to="/roadmap"
-          className={({ isActive }) => `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle({ isActive })}`}
-        >
-          Roadmap
-        </NavLink>
+            <NavLink
+              to="/roadmap"
+              className={({ isActive }) =>
+                `p-4 rounded-[10px] transition-all hover:text-white hover:gradients ${navLinkStyle(
+                  { isActive }
+                )}`
+              }
+            >
+              Roadmap
+            </NavLink>
           </nav>
         </SheetContent>
       </Sheet>
 
-      {isLoggedIn
-      ?(
-        <Button variant="default" onClick={()=> setIsLoggedIn(false)}>Log in</Button>
-      )
-      :(
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      {isLoggedIn ? (
+        <Button variant="default" onClick={() => setIsLoggedIn(false)}>
+          Log in
+        </Button>
+      ) : (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       )}
     </header>
   );
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -11,7 +11,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <MainLayout />,
+    element: <MainLayout />,
     errorElement: <p>Error</p>,
     children: [
       {
