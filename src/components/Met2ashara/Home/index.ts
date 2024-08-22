45c8b6f90d6c7fe014a3753af47cd73068fa41e7
Nabil Alex <nabilmohaed46@gmail.com>
2024-08-22 04:26:55 +0300

    add card item

--- src/components/Met2ashara/Home/index.ts
+++ src/components/Met2ashara/Home/index.ts
@@ -2,5 +2,6 @@ import Basics from "./Basics";
 import CarouselCard from "./CarouselCard";
 import Download from "./Download";
 import Hero from "./Hero";
+import CarouselCompanies from "./CarouselCompanies";
 
-export { Basics, CarouselCard, Download, Hero };
+export { Basics, CarouselCard, Download, Hero, CarouselCompanies };
