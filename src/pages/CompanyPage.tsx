5b3907fde7201a457498f5a6d02e720c0c9d4652
EHAB3x <ehabmah6969@gmail.com>
2024-08-23 13:25:13 +0200

    add job field to the card

--- src/pages/CompanyPage.tsx
+++ src/pages/CompanyPage.tsx
@@ -83,19 +83,22 @@ const CompanyPage = () => {
           <hr className="w-[50%] border-t-2 border-primary" />
         </div>
 
-        <div className="grid sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-4 my-6">
+        <div className="grid sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 grid-cols-1 sm:gap-4 md:gap-6 lg:gap-4 gap-6 my-6">
           {data.jobs.map((job: Ijobs, index: number) => (
             <div
               key={index}
-              className="border-primary border py-3 px-9 rounded-md flex gap-6"
+              className="border-primary border py-6 px-9 rounded-md flex gap-6 relative"
             >
+              <span className="inline-block bg-gradient-to-r from-sky-800 to-blue-900 text-white rounded-md px-3 py-1 text-sm font-semibold absolute top-[-14px] left-1/2 translate-x-[-50%]">
+                {job.field}
+              </span>
               <div className="flex items-center">
                 <img src={job.logo} alt={`${job.name} logo`} className="object-contain"/>
               </div>
-              <div className="flex flex-col justify-between">
+              <div className="flex flex-col justify-between gap-1">
                 <h2 className="font-bold">{job.name}</h2>
 
-                <div>
+                <div className="flex flex-col gap-1">
                   <div className="flex gap-2 items-center">
                     <MdEmail />
                     <p>{data.company.email}</p>
